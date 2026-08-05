1ba37bb2b37b074c03bfd4ed05dc43de94b03997
Niklas Tasler <niklastasler@gmail.com>
2026-08-05 20:26:50 +0200

    feat(monitoring): add alloy.collectByDefault option (fixes #2024)

--- modules/monitoring/extension.nix
+++ modules/monitoring/extension.nix
@@ -4,13 +4,17 @@
   ...
 }: let
   monitoringEnabled = config.nps.stacks.monitoring.enable;
+  defaultAlloyCollection = config.nps.stacks.monitoring.alloy.collectByDefault;
 in {
   # If a container has the logging label, add alloy
   options.services.podman.containers = lib.mkOption {
     type = lib.types.attrsOf (lib.types.submodule ({config, ...}: {
-      options.alloy.enable = lib.mkEnableOption ''
-        Alloy Log Scraping. If enabled, Alloy will scrape logs from the container and ship them to Loki.;
-      '';
+      options.alloy.enable = lib.mkOption {
+        type = lib.types.bool;
+        description = "Alloy Log Scraping. If enabled, Alloy will scrape logs from the container and ship them to Loki.;";
+        default = defaultAlloyCollection;
+        defaultText = lib.literalExpression ''config.nps.stacks.monitoring.alloy.collectByDefault'';
+      };
       config = lib.mkIf (monitoringEnabled && config.alloy.enable) {
         labels."logging.alloy" = "true";
       };
